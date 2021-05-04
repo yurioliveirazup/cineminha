a58f479101f7496a7fac5fbe2bfe4ed49d29375c
Yuri Matheus <yurimatheusb@hotmail.com>
2021-05-04 12:43:16 -0300

    inicia o projeto

---
+++ src/main/java/br/com/zup/cineminha/CineminhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.cineminha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineminhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineminhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/cineminha/CineminhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.cineminha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineminhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
